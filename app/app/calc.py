489ad4242daf0922ee81f5e75f36772fb73ce9e3
Sydrick Frisco <syd.frisco@gmail.com>
2023-05-03 13:33:00 +0800

    Adding Postgresql.

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,11 +2,11 @@
 Calculator functions
 """
 
-def add(x,y):
+def add(x, y):
     """ Add x and y return result."""
     return x + y
 
 
-def subtract(x,y):
+def subtract(x, y):
     """Subtract x and y return result."""
     return x - y
