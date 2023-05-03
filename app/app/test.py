9ab2168b5fbba686b10a00a5bbc01117f8d123d7
Sydrick Frisco <syd.frisco@gmail.com>
2023-05-03 13:28:31 +0800

    Adding Postgresql.

--- app/app/test.py
+++ app/app/test.py
@@ -11,12 +11,10 @@ class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         """Test adding numbers togother."""
         res = calc.add(5, 6)
-        
         self.assertEqual(res, 11)
         
     def test_subtract_numbers(self):
         """Test subtracting numbers."""
         res = calc.subtract(15, 10)
-        
         self.assertEqual(res, 5)
         
