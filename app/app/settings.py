d5bac705aa13cf71ea8339165526a6e9906279aa
Sydrick Frisco <syd.frisco@gmail.com>
2023-05-12 12:29:04 +0800

    Build User API

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,8 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #app
     'core',
+    'user',
     #package
     'rest_framework',
+    'rest_framework.authtoken',
     'drf_spectacular',
 ]
 
